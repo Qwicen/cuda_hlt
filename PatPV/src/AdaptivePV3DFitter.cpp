7dc81928371ab626d7d2cc34a08b1a8f27fa4c46
Florian Reiss <freiss@cern.ch>
2018-07-04 15:51:25 +0200

    write out cached matrixes

--- PatPV/src/AdaptivePV3DFitter.cpp
+++ PatPV/src/AdaptivePV3DFitter.cpp
@@ -15,8 +15,8 @@ struct State {
   double x = 0.;
   double y = 0.;
   double z = 0.;
-  double errX2 = 0.;
-  double errY2 = 0.;
+  double errX2 = 1.;
+  double errY2 = 1.;
 
 
 };
@@ -84,7 +84,7 @@ struct Vector2 {
   class AdaptivePVTrack 
   {
   public:
-    AdaptivePVTrack( Track& track) ;
+    AdaptivePVTrack( Track& track, XYZPoint& vtx) ;
     void updateCache( const XYZPoint& vtx ) ;
     double weight() const { return m_weight ; }
     void setWeight(double w) { m_weight = w ;}
@@ -108,7 +108,7 @@ struct Vector2 {
   } ;
 
 
-  AdaptivePVTrack::AdaptivePVTrack(Track& track)
+  AdaptivePVTrack::AdaptivePVTrack(Track& track, XYZPoint& vtx)
     : m_track(&track)
   {
     // get the state
@@ -119,28 +119,46 @@ struct Vector2 {
     m_H[(2 * (2 + 1)) / 2 + 0] = - m_state.tx ;
     m_H[(2 * (2 + 1)) / 2 + 1] = - m_state.ty ;
     // update the cache
-    //updateCache( vtx ) ;
+    updateCache( vtx ) ;
   }
 
-/*
+
   void AdaptivePVTrack::updateCache(const XYZPoint& vtx)
   {
     // transport to vtx z
-    m_state.linearTransportTo( vtx.z() ) ;
+    //m_state.linearTransportTo( vtx.z() ) ;
 
     // invert cov matrix
-    m_invcov = m_state.covariance().Sub<SymMatrix2x2>(0,0) ;
-    m_invcov.InvertChol() ;
+
+    //write out inverse covariance matrix
+    m_invcov[0] = 1. / m_state.errX2;
+    m_invcov[1] = 0.;
+    m_invcov[2] = 1. / m_state.errY2;
 
     // The following can all be written out, omitting the zeros, once
     // we know that it works.
-    Vector2 res{ vtx.x() - m_state.x(), vtx.y() - m_state.y() };
-    ROOT::Math::SMatrix<double,3,2> HW = m_H*m_invcov ;
-    ROOT::Math::AssignSym::Evaluate(m_halfD2Chi2DX2, HW*ROOT::Math::Transpose(m_H) ) ;
+    Vector2 res{ vtx.x - m_state.x, vtx.y - m_state.y };
+    double HW[6] ;
+    HW[0] = 1. / m_state.errX2;
+    HW[1] = 0.;
+    HW[2] = 1. / m_state.errY2;
+    HW[3] = - m_state.tx / m_state.errX2;
+    HW[4] = - m_state.ty / m_state.errY2;
+    HW[5] = 0.;
+    
+    m_halfD2Chi2DX2[0] = 1. / m_state.errX2;
+    m_halfD2Chi2DX2[1] = 0.;
+    m_halfD2Chi2DX2[2] = 1. / m_state.errY2;
+    m_halfD2Chi2DX2[3] = - m_state.tx / m_state.errX2;
+    m_halfD2Chi2DX2[4] = - m_state.ty / m_state.errY2;
+    m_halfD2Chi2DX2[5] = m_state.tx * m_state.tx / m_state.errX2 + m_state.ty * m_state.ty / m_state.errY2;
     //m_halfD2Chi2DX2 = ROOT::Math::Similarity(H, invcov ) ;
-    m_halfDChi2DX   = HW * res ;
-    m_chi2          = ROOT::Math::Similarity(res,m_invcov) ;
+    m_halfDChi2DX.x = res.x / m_state.errX2;
+    m_halfDChi2DX.y = res.y / m_state.errY2;
+    m_halfDChi2DX.z = -m_state.tx*res.x / m_state.errX2 -m_state.ty*res.y / m_state.errY2;
+    m_chi2          = res.x*res.x / m_state.errX2 +res.y*res.y / m_state.errY2;
   }
+/*
 
   inline double AdaptivePVTrack::chi2( const XYZPoint& vtx ) const
   {
