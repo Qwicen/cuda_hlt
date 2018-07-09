f0d5148ea92b63b4b17bf5cbf0774209929fba18
Dorothea vom Bruch <dorothea.vom.bruch@cern.ch>
2018-07-09 18:06:12 +0200

    allocate memory for host_velo_states

--- stream/sequence/src/Stream.cu
+++ stream/sequence/src/Stream.cu
@@ -45,6 +45,7 @@ cudaError_t Stream::initialize(
   cudaCheck(cudaMallocHost((void**)&host_total_number_of_velo_clusters, sizeof(uint)));
   cudaCheck(cudaMallocHost((void**)&host_number_of_reconstructed_velo_tracks, sizeof(uint)));
   cudaCheck(cudaMallocHost((void**)&host_accumulated_number_of_hits_in_velo_tracks, sizeof(uint)));
+  cudaCheck(cudaMallocHost((void**)&host_velo_states, max_number_of_events * VeloTracking::max_tracks * sizeof(VeloState)));
 
   // Define sequence of algorithms to execute
   sequence.set(sequence_algorithms());
