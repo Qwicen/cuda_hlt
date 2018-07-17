745671a7989c8a50d4defe75b40ca2256b3aa81e
Florian Reiss <freiss@cern.ch>
2018-07-17 18:19:37 +0200

    use Velo states from Kalman

--- stream/sequence/src/run_PatPV_CPU.cpp
+++ stream/sequence/src/run_PatPV_CPU.cpp
@@ -94,11 +94,47 @@ XYZPoint seed = seeds.at(0);
 fitter.fitVertex(seed, host_velo_states, recvtx, tracks2remove, number_of_events);
 */
 //Vertex  outvtxvec[100];
-std::vector<VeloState> velostate_vec;
+//std::vector<VeloState> velostate_vec;
 
-for(int i = 0; i < *host_number_of_tracks_pinned; i++)  velostate_vec.push_back(host_velo_states[i]); 
-reconstructMultiPVFromTracks(host_velo_states, outvtxvec, *host_number_of_tracks_pinned, number_of_vertex);
+//for(int i = 0; i < *host_number_of_tracks_pinned; i++)  velostate_vec.push_back(host_velo_states[i]); 
 
+
+for(int i_event = 0; i_event < number_of_events; i_event++) {
+
+  int number_of_tracks = host_number_of_tracks_pinned[i_event];
+ VeloState * state_base_pointer = host_velo_states + 2 * host_accumulated_tracks[i_event];
+VeloState  kalman_states[number_of_tracks];
+
+//recovers previusoly found vertices in first event
+for(int i = 0; i < number_of_tracks; i++) kalman_states[i] = state_base_pointer[2*i ];
+std::cout << "least: " << kalman_states[1].x <<std::endl;
+std::cout << "least: " << kalman_states[1].y <<std::endl;
+std::cout << "least: " << kalman_states[1].z <<std::endl;
+std::cout << "least: " << kalman_states[1].tx <<std::endl;
+std::cout << "least: " << kalman_states[1].ty <<std::endl;
+std::cout << "least: " << kalman_states[1].c00 <<std::endl;
+std::cout << "least: " << kalman_states[1].c20 <<std::endl;
+std::cout << "least: " << kalman_states[1].c22 <<std::endl;
+std::cout << "least: " << kalman_states[1].c11 <<std::endl;
+std::cout << "least: " << kalman_states[1].c31 <<std::endl;
+std::cout << "least: " << kalman_states[1].c33 <<std::endl;
+
+  //not workign yet
+for(int i = 0; i < number_of_tracks; i++) kalman_states[i] = state_base_pointer[2*i +1];
+std::cout << "kalman: " << kalman_states[1].x <<std::endl;
+std::cout << "kalman: " << kalman_states[1].y <<std::endl;
+std::cout << "kalman: " << kalman_states[1].z <<std::endl;
+std::cout << "kalman: " << kalman_states[1].tx <<std::endl;
+std::cout << "kalman: " << kalman_states[1].ty <<std::endl;
+std::cout << "kalman: " << kalman_states[1].c00 <<std::endl;
+std::cout << "kalman: " << kalman_states[1].c20 <<std::endl;
+std::cout << "kalman: " << kalman_states[1].c22 <<std::endl;
+std::cout << "kalman: " << kalman_states[1].c11 <<std::endl;
+std::cout << "kalman: " << kalman_states[1].c31 <<std::endl;
+std::cout << "least: " << kalman_states[1].c33 <<std::endl;
+
+reconstructMultiPVFromTracks(kalman_states, outvtxvec, host_number_of_tracks_pinned[i_event], number_of_vertex);
+}
 
   return 0;
 }
