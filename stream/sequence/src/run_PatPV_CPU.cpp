786b3fb2b245c1c87e0e0ed4f5c6b9811c6a4404
Florian Reiss <freiss@cern.ch>
2018-07-19 14:54:02 +0200

    start with simple PVChecker

--- stream/sequence/src/run_PatPV_CPU.cpp
+++ stream/sequence/src/run_PatPV_CPU.cpp
@@ -25,8 +25,10 @@ bool reconstructMultiPVFromTracks( VeloState * tracks2use,
 
 
   PVSeedTool seedtool;
-  double m_beamSpotX = 0.02;
-  double m_beamSpotY = -0.16;
+  //double m_beamSpotX = 0.02;
+  //double m_beamSpotY = -0.16;
+  double m_beamSpotX = 0.;
+  double m_beamSpotY = 0.;
   XYZPoint beamspot{m_beamSpotX, m_beamSpotY, 0.};
   
 
@@ -137,4 +139,120 @@ reconstructMultiPVFromTracks(kalman_states, outvtxvec, host_number_of_tracks_pin
 }
 
   return 0;
+}
+
+
+struct MCVertex {
+  double x;
+  double y;
+  double z;
+  int numberTracks;
+};
+
+
+void checkPVs(  const std::string& foldername,  const bool& fromNtuple, uint number_of_files)
+
+
+{
+   std::cout << "Checking PVs: " << std::endl;
+   std::vector<std::string> folderContents = list_folder(foldername, fromNtuple);
+  
+  uint requestedFiles = number_of_files==0 ? folderContents.size() : number_of_files;
+  verbose_cout << "Requested " << requestedFiles << " files" << std::endl;
+
+  if ( requestedFiles > folderContents.size() ) {
+    error_cout << "ERROR: requested " << requestedFiles << " files, but only " << folderContents.size() << " files are present" << std::endl;
+    exit(-1);
+  }
+
+  int readFiles = 0;
+  
+  //vector containing for each event vector of MCVertices
+  std::vector<std::vector<MCVertex>> events_vertices;
+  
+  for (uint i=0; i<requestedFiles; ++i) {
+    // Read event #i in the list and add it to the inputs
+    // if more files are requested than present in folder, read them again
+    std::string readingFile = folderContents[i % folderContents.size()];
+    std::string filename = foldername + "/" + readingFile;
+
+    // Check if file exists
+    if (!fileExists(filename)){
+    throw StrException("Error: File " + filename + " does not exist.");
+     }
+   
+    TFile *file = new TFile(filename.data(),"READ");
+    TTree *tree = (TTree*)file->Get("Hits_detectors");
+    assert( tree);
+
+    TBranch        *b_ovtx_x;   //!
+    TBranch        *b_ovtx_y;   //!
+    TBranch        *b_ovtx_z;   //!
+    TBranch        *b_p;
+    TBranch        *b_MCVertexType;
+    TBranch        *b_VertexNumberOfTracks;
+    TBranch        *b_MCVertexNumberOfTracks;
+
+
+    Double_t        ovtx_x;
+    Double_t        ovtx_y;
+    Double_t        ovtx_z;
+    Double_t        p;
+    Int_t           MCVertexType;
+    Int_t           VertexNumberOfTracks;
+    Int_t           MCVertexNumberOfTracks;
+    
+
+    TTree* fChain = tree;
+
+    fChain->SetBranchAddress("ovtx_x", &ovtx_x, &b_ovtx_x);
+    fChain->SetBranchAddress("ovtx_y", &ovtx_y, &b_ovtx_y);
+    fChain->SetBranchAddress("ovtx_z", &ovtx_z, &b_ovtx_z);
+    fChain->SetBranchAddress("p", &p, &b_p);
+    fChain->SetBranchAddress("MCVertexType", &MCVertexType, &b_MCVertexType);
+    fChain->SetBranchAddress("VertexNumberOfTracks", &VertexNumberOfTracks, &b_VertexNumberOfTracks);
+    fChain->SetBranchAddress("MCVertexNumberOfTracks", &MCVertexNumberOfTracks, &b_MCVertexNumberOfTracks);
+
+    // vector containing MC vertices
+    std::vector<MCVertex> vertices;
+
+    std::vector<double> found_z;
+    Long64_t maxEntries = fChain->GetTree()->GetEntries();
+    for(Long64_t entry = 0; entry< maxEntries ; ++entry){
+      fChain->GetTree()->GetEntry(entry);
+      if( p<0 || MCVertexType != 1) continue;  // Hits not associated to an MCP are stored with p < 0 and only look for trakcs from PVs
+
+      if(std::find(found_z.begin(), found_z.end(), ovtx_z) == found_z.end()) {
+        std::cout << "found a new vertex with: " << std::endl;
+        std::cout << "x: " << ovtx_x << std::endl;
+        std::cout << "y: " << ovtx_y << std::endl;
+        std::cout << "z: " << ovtx_z << std::endl;
+        std::cout << "number of tracks: " << MCVertexNumberOfTracks << std::endl;
+        found_z.push_back(ovtx_z);
+        MCVertex vertex;
+        vertex.x = ovtx_x;
+        vertex.y = ovtx_y;
+        vertex.z = ovtx_z;
+        vertex.numberTracks = MCVertexNumberOfTracks;
+        vertices.push_back(vertex);
+      }
+
+    }
+
+    events_vertices.push_back(vertices);
+  }
+  std::cout << "here are the vertices!" << std::endl;
+  for(auto vtx_vec : events_vertices) {
+    for (auto vtx : vtx_vec) {
+      std::cout << "x: " << vtx.x << std::endl;
+        std::cout << "y: " << vtx.y << std::endl;
+        std::cout << "z: " << vtx.z << std::endl;
+        std::cout << "number of tracks: " << vtx.numberTracks << std::endl;
+    }
+  }
+
+
+  //now, for each event, loop over MCVertices and check if it has been found (distance criterion) -> calculate efficiency
+
+
 }
