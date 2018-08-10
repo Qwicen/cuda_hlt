39f2c04308cea092efae161d96fbbeeb3b330585
Lars Funke <lars.funke@cern.ch>
2018-08-10 16:22:47 +0200

    Fixed FT geometry definition.

--- cuda/ft/common/include/FTDefinitions.cuh
+++ cuda/ft/common/include/FTDefinitions.cuh
@@ -11,14 +11,25 @@ struct FTGeometry {
   size_t size;
   uint32_t number_of_stations;
   uint32_t number_of_layers_per_station;
+  uint32_t number_of_quarters_per_layer;
   uint32_t number_of_layers;
-  uint32_t* number_of_modules; //for each layer
+  uint32_t number_of_quarters;
+  uint32_t* number_of_modules; //for each quarter
+  uint32_t number_of_mats_per_module;
+  uint32_t number_of_mats;
+  float* mirrorPoint_x;
+  float* mirrorPoint_y;
+  float* mirrorPoint_z;
+  float* ddx_x;
+  float* ddx_y;
+  float* ddx_z;
+  float* uBegin;
+  float* halfChannelPitch;
+  float* dieGap;
+  float* sipmPitch;
   float* dxdy;
   float* dzdy;
   float* globaldy;
-  float* endpoint_x;
-  float* endpoint_y;
-  float* endpoint_z;
 
   /**
    * @brief Typecast from std::vector.
--- cuda/ft/common/src/FTDefinitions.cu
+++ cuda/ft/common/src/FTDefinitions.cu
@@ -1,40 +1,27 @@
 #include "FTDefinitions.cuh"
 
-/*
-                   ---Geometry format:---
-| name                         | type     | size            |
-|------------------------------|----------|-----------------|
-| number_of_stations           | uint32_t | 1               |
-| number_of_layers_per_station | uint32_t | 1               |
-| number_of_modules            | uint32_t | number_of_layers|
-| dxdy                         | float    | number_of_layers|
-| dzdy                         | float    | number_of_layers|
-| globaldy                     | float    | number_of_layers|
-| endpoint_x                   | float    | number_of_layers|
-| endpoint_y                   | float    | number_of_layers|
-| endpoint_z                   | float    | number_of_layers|
+/*                     ---Geometry format---
+| name                         | type     | size               |
+|------------------------------|----------|--------------------|
+| number_of_stations           | uint32_t | 1                  |
+| number_of_layers_per_station | uint32_t | 1                  |
+| number_of_quarters_per_layer | uint32_t | 1                  |
+| number_of_modules            | uint32_t | number_of_quarters |
+| number_of_mats_per_module    | uint32_t | 1                  |
+| mirrorPoint_x                | float    | number_of_mats     |
+| mirrorPoint_y                | float    | number_of_mats     |
+| mirrorPoint_z                | float    | number_of_mats     |
+| ddx_x                        | float    | number_of_mats     |
+| ddx_y                        | float    | number_of_mats     |
+| ddx_z                        | float    | number_of_mats     |
+| uBegin                       | float    | number_of_mats     |
+| halfChannelPitch             | float    | number_of_mats     |
+| dieGap                       | float    | number_of_mats     |
+| sipmPitch                    | float    | number_of_mats     |
+| dxdy                         | float    | number_of_mats     |
+| dzdy                         | float    | number_of_mats     |
+| globaldy                     | float    | number_of_mats     |
 */
-
-FTGeometry::FTGeometry(const std::vector<char>& geometry) {
-  const char* p = geometry.data();
-
-  number_of_stations           = *((uint32_t*)p); p += sizeof(uint32_t);
-  number_of_layers_per_station = *((uint32_t*)p); p += sizeof(uint32_t);
-  number_of_layers             = number_of_stations * number_of_layers_per_station;
-  number_of_modules            = (uint32_t*)p; p += number_of_layers * sizeof(uint32_t);
-  dxdy                         = (float*)p; p += sizeof(float) * number_of_layers;
-  dzdy                         = (float*)p; p += sizeof(float) * number_of_layers;
-  globaldy                     = (float*)p; p += sizeof(float) * number_of_layers;
-  endpoint_x                   = (float*)p; p += sizeof(float) * number_of_layers;
-  endpoint_y                   = (float*)p; p += sizeof(float) * number_of_layers;
-  endpoint_z                   = (float*)p; p += sizeof(float) * number_of_layers;
-
-  size = p - geometry.data();
-
-  if (size != geometry.size()) {
-    std::cout << "Size mismatch for geometry" << std::endl;
-  }
-}
 
 __device__ __host__ FTGeometry::FTGeometry(
   const char* geometry
@@ -43,14 +30,29 @@ __device__ __host__ FTGeometry::FTGeometry(
 
   number_of_stations           = *((uint32_t*)p); p += sizeof(uint32_t);
   number_of_layers_per_station = *((uint32_t*)p); p += sizeof(uint32_t);
+  number_of_quarters_per_layer = *((uint32_t*)p); p += sizeof(uint32_t);
   number_of_layers             = number_of_stations * number_of_layers_per_station;
-  number_of_modules            = (uint32_t*)p; p += number_of_layers * sizeof(uint32_t);
-  dxdy                         = (float*)p; p += sizeof(float) * number_of_layers;
-  dzdy                         = (float*)p; p += sizeof(float) * number_of_layers;
-  globaldy                     = (float*)p; p += sizeof(float) * number_of_layers;
-  endpoint_x                   = (float*)p; p += sizeof(float) * number_of_layers;
-  endpoint_y                   = (float*)p; p += sizeof(float) * number_of_layers;
-  endpoint_z                   = (float*)p; p += sizeof(float) * number_of_layers;
+  number_of_quarters           = number_of_quarters_per_layer * number_of_layers;
+  number_of_modules            = (uint32_t*)p; p += number_of_quarters * sizeof(uint32_t);
+  number_of_mats_per_module    = *((uint32_t*)p); p += sizeof(uint32_t);
+  number_of_mats               = 0;
+  for(size_t quarter = 0; quarter < number_of_quarters; quarter++)
+    number_of_mats += number_of_modules[quarter] * number_of_mats_per_module;
+  mirrorPoint_x                = (float*)p; p += sizeof(float) * number_of_mats;
+  mirrorPoint_y                = (float*)p; p += sizeof(float) * number_of_mats;
+  mirrorPoint_z                = (float*)p; p += sizeof(float) * number_of_mats;
+  ddx_x                        = (float*)p; p += sizeof(float) * number_of_mats;
+  ddx_y                        = (float*)p; p += sizeof(float) * number_of_mats;
+  ddx_z                        = (float*)p; p += sizeof(float) * number_of_mats;
+  uBegin                       = (float*)p; p += sizeof(float) * number_of_mats;
+  halfChannelPitch             = (float*)p; p += sizeof(float) * number_of_mats;
+  dieGap                       = (float*)p; p += sizeof(float) * number_of_mats;
+  sipmPitch                    = (float*)p; p += sizeof(float) * number_of_mats;
+  dxdy                         = (float*)p; p += sizeof(float) * number_of_mats;
+  dzdy                         = (float*)p; p += sizeof(float) * number_of_mats;
+  globaldy                     = (float*)p; p += sizeof(float) * number_of_mats;
 
   size = p - geometry;
 }
+
+FTGeometry::FTGeometry(const std::vector<char>& geometry) : FTGeometry::FTGeometry(geometry.data()) {}
