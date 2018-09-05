79e4a39284f0b54156600439740b93b4f1d203b5
Florian Reiss <freiss@cern.ch>
2018-09-05 11:29:35 +0200

    first version of nominal PV checker -> still something wrong with nubmer of RecTracks for each MC PV

--- stream/sequence/src/PrimaryVertexChecker.cpp
+++ stream/sequence/src/PrimaryVertexChecker.cpp
@@ -31,7 +31,17 @@ void checkPVs(  const std::string& foldername,  const bool& fromNtuple, uint num
 
 
 // vector containing MC vertices
-    std::vector<MCVertex> vertices;
+    //std::vector<MCVertex> vertices;
+
+ //counters for efficiencies/fake rate
+  int m_nMCPV = 0;
+  int m_nRecMCPV  = 0;
+  int m_nMCPV_isol = 0;
+  int m_nRecMCPV_isol = 0;
+  int m_nMCPV_close = 0;
+  int m_nRecMCPV_close = 0;
+  int m_nFalsePV = 0;
+  int m_nFalsePV_real = 0;
   
   //loop over files/events
   for (uint i_event=0; i_event<requestedFiles; ++i_event) {
@@ -58,7 +68,7 @@ void checkPVs(  const std::string& foldername,  const bool& fromNtuple, uint num
       mc_vertex.y = *((double*)  input); input += sizeof(double);
       mc_vertex.z = *((double*)  input); input += sizeof(double);
 
-      if(mc_vertex.numberTracks >= 4) vertices.push_back(mc_vertex);
+      //if(mc_vertex.numberTracks >= 4) vertices.push_back(mc_vertex);
       MC_vertices.push_back(mc_vertex);
     }
     
@@ -107,12 +117,241 @@ for (auto vtx : MC_vertices) {
      for (auto fake : isFake) {if (fake) number_fake_vertices++;};
 
 
-    events_vertices.push_back(vertices);
+    //events_vertices.push_back(vertices);
+
+
+  //try to implement nominal PV checker
+
+  std::vector<Vertex*> vecOfVertices;
+  //first fill vector with vertices
+  for(uint i = 0; i < number_of_vertex[i_event]; i++) {
+    int index = i_event  * PatPV::max_number_vertices + i;
+    vecOfVertices.push_back(&(rec_vertex[index]));
+  }
+  // Fill reconstucted PV info
+  std::vector<RecPVInfo> recpvvec;
+  std::vector<Vertex*>::iterator itRecV;
+  for(itRecV = vecOfVertices.begin(); vecOfVertices.end() != itRecV;
+               itRecV++) {
+    Vertex* pv;
+    pv = *itRecV;
+    RecPVInfo recinfo;
+    recinfo.pRECPV= pv;
+    recinfo.x = pv->x;
+    recinfo.y = pv->y;
+    recinfo.z = pv->z;
+
+    
+    double sigx = sqrt(pv->cov00);
+    double sigy = sqrt(pv->cov11);
+    double sigz = sqrt(pv->cov22);
+    XYZPoint a3d(sigx,sigy,sigz);
+    recinfo.positionSigma = a3d;
+    recinfo.nTracks = pv->tracks.size();
+    double minRD = 99999.;
+    double maxRD = -99999.;
+    double chi2 = pv->chi2;
+    double nDoF = pv->ndof;
+
+
+    
+
+    int mother = 0;
+    int velo = 0;
+    int lg = 0;
+    double d0 = 0;
+    double mind0 = 99999.0;
+    double maxd0 = -99999.0;
+    double trackChi2 = 0.0;
+    int tr = 0;
+
+    
+
+   
+
+
+    recinfo.minTrackRD = minRD;
+    recinfo.maxTrackRD = maxRD;
+    recinfo.mother = mother;
+    recinfo.chi2 = chi2;
+    recinfo.nDoF = nDoF;
+    recinfo.d0 = d0;
+    recinfo.d0nTr = (double)d0/(double)tr;
+    recinfo.chi2nTr = (double)trackChi2/(double)tr; 
+    recinfo.mind0 = mind0;
+    recinfo.maxd0 = maxd0;
+    recinfo.nVeloTracks = velo; 
+    recinfo.nLongTracks = lg; 
+    recinfo.indexMCPVInfo = -1;
+    recpvvec.push_back(recinfo);
+    
+  }
+
+    // Fill MC PV info
+  //convert vector of MC PVs to vector of pointer to MC PVs
+
+  //do checking of collision type and mother here or in dumping?
+  std::vector<MCVertex*> ptr_MC_vertices;
+  for(auto mc_vtx : MC_vertices) ptr_MC_vertices.push_back(&mc_vtx);
+  
+  //vecotr with MCPVinfo
+ std::vector<MCPVInfo> mcpvvec;
+                   
+  for(std::vector<MCVertex*>::const_iterator itMCV = ptr_MC_vertices.begin();
+      ptr_MC_vertices.end() != itMCV; itMCV++) {
+    
+    
+      
+        MCPVInfo mcprimvert;
+        mcprimvert.pMCPV = *itMCV;
+        //mcprimvert.nRecTracks = 0;
+        mcprimvert.nRecTracks = (*itMCV)->numberTracks;
+        mcprimvert.nRecBackTracks = 0;
+        mcprimvert.indexRecPVInfo = -1;
+        mcprimvert.nCorrectTracks = 0;
+        mcprimvert.multClosestMCPV = 0;
+        mcprimvert.distToClosestMCPV = 999999.;
+        mcprimvert.decayBeauty = 0;
+        mcprimvert.decayCharm  = 0;
+        
+        mcpvvec.push_back(mcprimvert);
+      
+    
+  }
+  std::cout << "nubmer of MCPVinfo:" << i_event << " " << mcpvvec.size() << std::endl;
+
+  std::vector<MCPVInfo> rblemcpv;
+  std::vector<MCPVInfo> not_rble_but_visible;
+  std::vector<MCPVInfo> not_rble;
+  int nmrc = 0;
+
+  //configuration for PV checker -> check values
+int m_nTracksToBeRecble = 5;
+double m_dzIsolated = 10;
+
+  std::vector<MCPVInfo>::iterator itmc;
+  for (itmc = mcpvvec.begin(); mcpvvec.end() != itmc; itmc++) {
+    rblemcpv.push_back(*itmc);
+    std::cout << "number of tracks: " << (*itmc).nRecTracks << std::endl;;
+    if (itmc->nRecTracks < m_nTracksToBeRecble)
+      {
+        nmrc++;
+      }
+    if(itmc->nRecTracks < m_nTracksToBeRecble && itmc->nRecTracks > 1)
+      {
+        not_rble_but_visible.push_back(*itmc);
+      }
+    if(itmc->nRecTracks < m_nTracksToBeRecble && itmc->nRecTracks < 2)
+      {
+        not_rble.push_back(*itmc);  
+      }
+
+  }
+  std::cout << "nubmer of rblemcpv:" << i_event << " " << rblemcpv.size() << std::endl;
+  std::cout << "nubmer of nmrc:" << i_event << " " << nmrc << std::endl;
+
+      for(int ipv = 0; ipv < (int) recpvvec.size(); ipv++) {
+      match_mc_vertex_by_distance(ipv, recpvvec, rblemcpv);
+    };
+
+
+    // find nr of false PV
+
+  int nFalsePV = 0;
+  int nFalsePV_real = 0;
+  for(int ipv = 0; ipv < (int) recpvvec.size(); ipv++) {
+    int fake = 0;
+    double x = recpvvec[ipv].x;
+    double y = recpvvec[ipv].y;
+    double z = recpvvec[ipv].z;
+    double r = std::sqrt(x*x + y*y);
+    double errx = recpvvec[ipv].positionSigma.x;
+    double erry = recpvvec[ipv].positionSigma.y;
+    double errz = recpvvec[ipv].positionSigma.z;
+    double errr = std::sqrt(((x*errx)*(x*errx)+(y*erry)*(y*erry))/(x*x+y*y));
+    double minRDTrack = recpvvec[ipv].minTrackRD;
+    double maxRDTrack = recpvvec[ipv].maxTrackRD;
+    int mother = recpvvec[ipv].mother;
+    double velo = recpvvec[ipv].nVeloTracks;
+    double lg = recpvvec[ipv].nLongTracks;
+    double d0 = recpvvec[ipv].d0;
+    double d0nTr = recpvvec[ipv].d0nTr;
+    double chi2nTr = recpvvec[ipv].chi2nTr;
+    double mind0 = recpvvec[ipv].mind0;
+    double maxd0 = recpvvec[ipv].maxd0;
+    double chi2 = recpvvec[ipv].chi2;
+    double nDoF = recpvvec[ipv].nDoF;
+
+
+    if (recpvvec[ipv].indexMCPVInfo < 0 ) {
+      nFalsePV++; 
+      fake = 1;
+      bool vis_found = false;
+      for(unsigned int imc = 0; imc < not_rble_but_visible.size() ; imc++) {
+        if ( not_rble_but_visible[imc].indexRecPVInfo > -1 ) continue;
+        double dist = fabs(mcpvvec[imc].pMCPV->z - recpvvec[ipv].z);
+        if (  dist < 5.0 * recpvvec[ipv].positionSigma.z ) {
+          vis_found = true;
+    not_rble_but_visible[imc].indexRecPVInfo = 10;
+          break;
+  }
+      } // imc
+      if ( !vis_found ) nFalsePV_real++;
+    }
+ }
+
+
+   // Counters
+  int nMCPV                 = rblemcpv.size()-nmrc;
+  int nRecMCPV              = 0;
+  int nMCPV_isol            = 0;
+  int nRecMCPV_isol         = 0;
+  int nMCPV_close           = 0;
+  int nRecMCPV_close        = 0;
+
+
+
+  for(itmc = rblemcpv.begin(); rblemcpv.end() != itmc; itmc++) {
+    if(itmc->distToClosestMCPV > m_dzIsolated) nMCPV_isol++;
+    if(itmc->distToClosestMCPV < m_dzIsolated) nMCPV_close++;
+    if(itmc->indexRecPVInfo > -1) {
+      nRecMCPV++;
+      if(itmc->distToClosestMCPV > m_dzIsolated) nRecMCPV_isol++;
+      if(itmc->distToClosestMCPV < m_dzIsolated) nRecMCPV_close++;
+    }
+  }
+
+    m_nMCPV                 +=  nMCPV;
+  m_nRecMCPV              +=  nRecMCPV;
+  m_nMCPV_isol            +=  nMCPV_isol;
+  m_nRecMCPV_isol         +=  nRecMCPV_isol;
+  m_nMCPV_close           +=  nMCPV_close;
+  m_nRecMCPV_close        +=  nRecMCPV_close;
+  m_nFalsePV              +=  nFalsePV;
+  m_nFalsePV_real         +=  nFalsePV_real;
+
+
+  MC_vertices.clear();
   } //end loop over files/events
   
 
   std::cout << "found " << number_reconstructed_vertices << " / " << number_reconstructible_vertices << " vertices! -> efficiency: " << (double)number_reconstructed_vertices / (double)number_reconstructible_vertices << std::endl; 
   std::cout << "fakes: " << number_fake_vertices << std::endl;
+
+
+
+            printRat("All",       m_nRecMCPV ,       m_nMCPV );
+            printRat("Isolated",  m_nRecMCPV_isol,   m_nMCPV_isol );
+            printRat("Close",     m_nRecMCPV_close,  m_nMCPV_close );
+            printRat("False rate",m_nFalsePV ,       m_nRecMCPV+m_nFalsePV );
+
+
+          printRat("Real false rate", m_nFalsePV_real , m_nRecMCPV+m_nFalsePV_real);
+
+   std::cout <<   "new found: " <<     m_nRecMCPV << " / " << m_nMCPV  << std::endl;
+   std::cout << "new fakes: " << m_nFalsePV << std::endl;
+
+          
     
     
     
--- stream/sequence/src/PrimaryVertexChecker.h
+++ stream/sequence/src/PrimaryVertexChecker.h
@@ -19,3 +19,82 @@ struct MCVertex {
   double z;
   int numberTracks;
 };
+
+typedef struct {  
+  MCVertex* pMCPV;     // pointer to MC PV 
+  int nRecTracks;            // number of reconstructed tracks from this MCPV  
+  int nRecBackTracks;        // number of reconstructed backward tracks
+  int indexRecPVInfo;        // index to reconstructed PVInfo (-1 if not reco)
+  int nCorrectTracks;        // correct tracks belonging to reconstructed PV
+  int multClosestMCPV;       // multiplicity of closest reconstructable MCPV
+  double distToClosestMCPV;  // distance to closest reconstructible MCPV
+  int decayCharm;             // type of mother particle                                                                                                                                           
+  int decayBeauty;
+  //std::vector<LHCb::MCParticle*> m_mcPartInMCPV;
+  //std::vector<LHCb::Track*> m_recTracksInMCPV;
+} MCPVInfo;
+
+typedef struct {
+ public:
+  int nTracks;                    // number of tracks
+  int nVeloTracks;                // number of velo tracks in a vertex       
+  int nLongTracks;
+  double minTrackRD;              //                                                                                                                                        
+  double maxTrackRD;              //                                                                             
+  double chi2;
+  double nDoF;
+  double d0;
+  double d0nTr;
+  double chi2nTr;
+  double mind0;
+  double maxd0;
+  int mother;
+  //XYZPoint position;       // position
+  double x;
+  double y;
+  double z;
+  XYZPoint positionSigma;  // position sigmas
+  int indexMCPVInfo;              // index to MCPVInfo
+  Vertex* pRECPV;        // pointer to REC PV
+} RecPVInfo;
+
+
+void match_mc_vertex_by_distance(int ipv, std::vector<RecPVInfo>& rinfo, std::vector<MCPVInfo>& mcpvvec) {
+
+  double mindist = 999999.;
+  int indexmc = -1;
+
+  for(int imc = 0; imc < (int) mcpvvec.size(); imc++) {
+    if ( mcpvvec[imc].indexRecPVInfo  > -1) continue;
+    double dist = fabs(mcpvvec[imc].pMCPV->z -
+                       rinfo[ipv].z);
+    if(dist < mindist) {
+      mindist = dist;
+      indexmc = imc;
+    }
+  }
+  if ( indexmc > -1 ) {
+    if(mindist < 5.0 * rinfo[ipv].positionSigma.z) {
+      rinfo[ipv].indexMCPVInfo = indexmc;
+      mcpvvec[indexmc].indexRecPVInfo = ipv;
+    }
+  }
+
+}
+
+void printRat(std::string mes, int a, int b) {
+
+  double rat = 0.;
+  if(b>0) rat = 1.0*a/b;
+
+  // reformat message
+  unsigned int len = 20;
+  std::string pmes = mes;
+  while(pmes.length() < len) {
+    pmes+=" ";
+  }
+  pmes+= " : ";
+
+  std::cout << pmes << " " << rat << std::endl;
+
+}
