484ef3dbffd446f990927bf6bbe112cb2ec767a5
Florian Reiss <freiss@cern.ch>
2018-09-06 14:34:41 +0200

    only accept rec vertex if it not too close to already reconstructed one

--- stream/sequence/src/run_PatPV_CPU.cpp
+++ stream/sequence/src/run_PatPV_CPU.cpp
@@ -3,6 +3,12 @@
 #include "PVSeedTool.h"
 
 
+//configuration
+
+//separation to accept reconstructed PV
+  double m_pvsChi2Separation = 25.;
+  double m_pvsChi2SeparationLowMult = 91;
+
 
 
 bool reconstructMultiPVFromTracks(VeloState * tracks2use, Vertex * outvtxvec, int host_number_of_tracks_pinned,
@@ -39,11 +45,50 @@ bool reconstructMultiPVFromTracks(VeloState * tracks2use, Vertex * outvtxvec, in
 
 
       // fitting
-      bool scvfit = fitVertex( seed, rtracks, recvtx, host_number_of_tracks_pinned, tracks2disable);
+
+      bool tracks2remove[host_number_of_tracks_pinned];
+
+      for(int i = 0; i < host_number_of_tracks_pinned; i++) tracks2remove[i] = false;
+
+
+      bool scvfit = fitVertex( seed, rtracks, recvtx, host_number_of_tracks_pinned, tracks2disable, tracks2remove);
       if (!scvfit) continue;
 
       
-      
+     
+
+  //only accept vertex if it is not too close too already found one
+
+ 
+
+  double chi2min = 1e10;
+  for(int i = 0; i < nvtx_after; i++) {
+    int index = event_number * PatPV::max_number_vertices + i;
+    double z1 = outvtxvec[index].z;
+    double z2 = recvtx.z;
+    double sigma2z1 = outvtxvec[index].cov22;
+    double sigma2z2 = recvtx.cov22;
+    double chi2 = pow((z1 - z2), 2) / (sigma2z1 + sigma2z2);
+    if (chi2 < chi2min) chi2min = chi2;
+  }
+
+
+
+  bool vsepar = true;
+
+
+  if ( chi2min < m_pvsChi2Separation ) vsepar = false;
+  // protect secondary vertices of B signal
+  //if ( chi2min < m_pvsChi2SeparationLowMult && recvtx.ndof < 7 ) vsepar = false;
+  if ( chi2min < m_pvsChi2SeparationLowMult && 0.5*(recvtx.ndof+3) < 7 ) vsepar = false;
+
+  if(!vsepar) continue;
+  
+
+
+      for(int i = 0; i < host_number_of_tracks_pinned; i++) {
+        if(tracks2remove[i]) tracks2disable[i] = true;
+      }
 
       
       outvtxvec[event_number * PatPV::max_number_vertices + nvtx_after] = recvtx;
@@ -101,9 +146,10 @@ XYZPoint beamspot = {0.,0.,0.};
 reconstructMultiPVFromTracks(kalman_states, outvtxvec, host_number_of_tracks_pinned[i_event], number_of_vertex, i_event, tracks2disable, seeds, number_of_seeds);
 }
 
+int total_number_seed = 0;
+for(int i_event = 0; i_event < number_of_events; i_event++)   total_number_seed += number_of_seeds[i_event];
 
-
-
+ std::cout << "total nubmer seeds in all events: " << total_number_seed << std::endl;
   return 0;
 }
 
