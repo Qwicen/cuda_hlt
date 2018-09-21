d27a118007da802f5b2498f6ac1d0801695ee20c
Sergey Popov <popovsergey95@gmail.com>
2018-09-21 18:36:33 +0300

    cmake find_package fix

--- stream/Findflatbuffers.cmake
+++ stream/Findflatbuffers.cmake
@@ -8,7 +8,7 @@ INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(flatbuffers 
   DEFAULT_MSG flatbuffers_INCLUDE_DIR flatbuffers_FLATC_EXECUTABLE)
 
-mark_as_advanced(flatbuffers_FOUND flatbuffers_INCLUDE_DIR)
-mark_as_advanced(flatbuffers_EXEC_FOUND flatbuffers_FLATC_EXECUTABLE)
-set(flatbuffers_INCLUDE_DIRS ${flatbuffers_INCLUDE_DIR})
-set(flatbuffers_FLATC ${flatbuffers_FLATC_EXECUTABLE})
+if(flatbuffers_FOUND)
+  set(flatbuffers_INCLUDE_DIRS ${flatbuffers_INCLUDE_DIR})
+  set(flatbuffers_FLATC ${flatbuffers_FLATC_EXECUTABLE})
+endif()
