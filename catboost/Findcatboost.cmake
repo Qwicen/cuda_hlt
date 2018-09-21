7085fbb25bb948691549bee3fd72e00dbf7e87c4
Sergey Popov <popovsergey95@gmail.com>
2018-09-21 19:19:24 +0300

    remove fbs files from repo

---
+++ catboost/Findcatboost.cmake
@@ -0,0 +1,14 @@
+find_path(catboost_INCLUDE_FBS NAMES model.fbs
+          HINTS ${catboost_home}/catboost/libs/model/flatbuffers)
+
+find_path(catboost_INCLUDE_DIR NAMES standalone_evaluator
+          HINTS ${catboost_home}/catboost/libs)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(catboost 
+  DEFAULT_MSG catboost_INCLUDE_FBS catboost_INCLUDE_DIR)
+
+if(catboost_FOUND)
+  set(catboost_FBS ${catboost_INCLUDE_FBS})
+  set(catboost_INCLUDE_DIRS ${catboost_INCLUDE_DIR})
+endif()
