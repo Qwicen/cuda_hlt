34e3ddf302e146ed88cf19f78de0df24c647e355
Daniel Campora <dcampora@cern.ch>
2018-09-22 18:15:16 +0200

    Updated event model to match new offset definition.

--- cuda/velo/common/src/ClusteringDefinitions.cu
+++ cuda/velo/common/src/ClusteringDefinitions.cu
@@ -5,7 +5,7 @@ __device__ __host__ VeloRawEvent::VeloRawEvent(
 ) {
   const char* p = event;
   number_of_raw_banks = *((uint32_t*)p); p += sizeof(uint32_t);
-  raw_bank_offset = (uint32_t*) p; p += number_of_raw_banks * sizeof(uint32_t);
+  raw_bank_offset = (uint32_t*) p; p += (number_of_raw_banks + 1) * sizeof(uint32_t);
   payload = (char*) p;
 }
 
