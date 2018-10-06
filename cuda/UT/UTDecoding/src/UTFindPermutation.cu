4db7590378e51b142eeee97ba2ab6d74565e18bc
Daniel Campora <dcampora@cern.ch>
2018-10-06 18:24:28 +0200

    Fixed bug in UTFindPermutation.
    * We need to refactor findPermutation to allow using shared memory
      arrays. For now, a custom implementation is done in UTFindPermutation.

--- cuda/UT/UTDecoding/src/UTFindPermutation.cu
+++ cuda/UT/UTDecoding/src/UTFindPermutation.cu
@@ -18,24 +18,20 @@ __global__ void ut_find_permutation(
   const UTHits ut_hits {dev_ut_hits, dev_ut_hit_offsets[number_of_events * number_of_unique_x_sectors]};
 
   // // Prints out all hits
-  // if (threadIdx.x == 0 && threadIdx.y == 0) {
-  //   for (int i=0; i<4; ++i) {
-  //     printf("Layer %i hits:\n", i);
-  //     for (int s=dev_unique_x_sector_layer_offsets[i]; s<dev_unique_x_sector_layer_offsets[i+1]; ++s) {
-  //       printf(" Sector group %i, x %f:\n", s, dev_unique_sector_xs[s]);
-  //       uint group_offset = ut_hit_offsets.sector_group_offset(s);
-  //       uint n_hits_group = ut_hit_offsets.sector_group_number_of_hits(s);
-  //       for (int j=0; j<n_hits_group; ++j) {
-  //         const auto hit_index = group_offset + j;
-  //         printf("  yBegin = %f, yEnd = %f, zAtYEq0 = %f, xAtYEq0 = %f, weight = %f, highThreshold = %u \n",
-  //          ut_hits.yBegin[hit_index],
-  //          ut_hits.yEnd[hit_index],
-  //          ut_hits.zAtYEq0[hit_index],
-  //          ut_hits.xAtYEq0[hit_index],
-  //          ut_hits.weight[hit_index],
-  //          ut_hits.highThreshold[hit_index]);
-  //       }
-  //     }
+  // if (blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 0) {
+  //   printf(" Sector group %i, x %f:\n", sector_group_number, dev_unique_sector_xs[sector_group_number]);
+  //   uint group_offset = ut_hit_offsets.sector_group_offset(sector_group_number);
+  //   uint n_hits_group = ut_hit_offsets.sector_group_number_of_hits(sector_group_number);
+  //   for (int j=0; j<n_hits_group; ++j) {
+  //     const auto hit_index = group_offset + j;
+  //     // printf("  yBegin = %f, yEnd = %f, zAtYEq0 = %f, xAtYEq0 = %f, weight = %f, highThreshold = %u \n",
+  //     //  ut_hits.yBegin[hit_index],
+  //     //  ut_hits.yEnd[hit_index],
+  //     //  ut_hits.zAtYEq0[hit_index],
+  //     //  ut_hits.xAtYEq0[hit_index],
+  //     //  ut_hits.weight[hit_index],
+  //     //  ut_hits.highThreshold[hit_index]);
+  //     printf("  yBegin = %f\n", ut_hits.yBegin[hit_index]);
   //   }
   // }
 
@@ -43,6 +39,7 @@ __global__ void ut_find_permutation(
   const uint sector_group_number_of_hits = ut_hit_offsets.sector_group_number_of_hits(sector_group_number);
 
   if (sector_group_number_of_hits > 0) {
+    // Load yBegin into a shared memory container
     // TODO: Find a proper maximum and cover corner cases
     __shared__ float s_y_begin [256];
     assert(sector_group_number_of_hits < 256);
@@ -53,13 +50,27 @@ __global__ void ut_find_permutation(
 
     __syncthreads();
 
-    find_permutation(
-      0,
-      dev_hit_permutations + sector_group_offset,
-      sector_group_number_of_hits,
-      [] (const int a, const int b) -> int {
-        return (s_y_begin[a] > s_y_begin[b]) - (s_y_begin[a] < s_y_begin[b]);
+    // Note: This could be a specialization of find_permutation,
+    //       but we would need to parameterize the hit_start in sector_group.
+    //       At this stage, it would make sense perhaps to have one permutation
+    //       specialization for shared memory cases, and one for global cases.
+
+    // Sort according to the natural order in s_y_begin
+    // Store the permutation found into dev_hit_permutations
+    const auto sort_function = [] (const int a, const int b) -> int {
+      return (s_y_begin[a] > s_y_begin[b]) - (s_y_begin[a] < s_y_begin[b]);
+    };
+
+    for (uint i=threadIdx.x; i<sector_group_number_of_hits; i+=blockDim.x) {
+      const int hit_index = i;
+      uint position = 0;
+      for (uint j = 0; j < sector_group_number_of_hits; ++j) {
+        const int other_hit_index = j;
+        const int sort_result = sort_function(hit_index, other_hit_index);
+        position += sort_result>0 || (sort_result==0 && i>j);
       }
-    );
+      assert(position < sector_group_number_of_hits);
+      dev_hit_permutations[sector_group_offset + position] = sector_group_offset + hit_index; 
+    }
   }
 }
