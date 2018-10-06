196bd08abf8900b5dc374b0b143615f959b12de4
Placido Fernandez Declara <placido.fernandez@cern.ch>
2018-10-06 20:44:14 +0200

    generic p-kick method for best hits

--- cuda/UT/compassUT/src/compassUT.cu
+++ cuda/UT/compassUT/src/compassUT.cu
@@ -209,13 +209,9 @@ __host__ __device__ std::tuple<int,int,int,int> find_best_hits(
       layers[i_layer] = N_LAYERS - 1 - i_layer;
   }
 
-  bool fourLayerSolution = false;
-  int hitCandidateIndices[N_LAYERS];
-  const float yyProto = velo_state.y - velo_state.ty * velo_state.z;
-
   // Get windows of all layers
-  window_indicator windows_ranges(dev_windows_layers); 
-  const auto* ranges = windows_ranges.get_track_candidates(i_track);
+  WindowIndicator win_ranges(dev_windows_layers); 
+  const auto* ranges = win_ranges.get_track_candidates(i_track);
   const int from0 = ranges->layer[0].first;
   const int to0 = ranges->layer[0].last;
   const int from2 = ranges->layer[2].first;
@@ -225,29 +221,31 @@ __host__ __device__ std::tuple<int,int,int,int> find_best_hits(
   const int from3 = ranges->layer[3].first;
   const int to3 = ranges->layer[3].last;
   
-  printf("from0: %i, to0: %i, from1: %i, to1: %i, from2: %i, to2: %i, from3: %i, to3: %i\n", from0, to0, from1, to1, from2, to2, from3, to3);
+  // printf("from0: %i, to0: %i, from1: %i, to1: %i, from2: %i, to2: %i, from3: %i, to3: %i\n", from0, to0, from1, to1, from2, to2, from3, to3);
 
-  int hit0 = -1, hit1 = -1, hit2 = -1, hit3 = -1;
-  float fit = FLT_MAX; //MAX_FLOAT
+  const float yyProto = velo_state.y - velo_state.ty * velo_state.z;
+
+  int best_hits[4] = {-1, -1, -1, -1};
+  // float fit = FLT_MAX; //MAX_FLOAT
 
   // auto is_valid = [](float dx, int layer, float y){
   //   if (dx < )
   // }
 
-    // const float normFactNum = normFact[layer];
-    // const float invNormFact = 1.0/normFactNum;
-    // xTol*invNormFact
-    // const auto zInit = ut_hits.zAtYEq0[layer_offset + posBeg];
-    // const auto xOnTrackProto = myState.x + myState.tx*(zInit - myState.z);
-    // const auto yApprox = myState.y + myState.ty * (zInit - myState.z);
-    // const auto xx = ut_hits.xAt(layer_offset + i, yApprox, dxDy); 
-    // const auto dx = xx - xOnTrackProto;
-    
-    // if( dx < -xTolNormFact ) continue;
-    // if( dx >  xTolNormFact ) continue; 
-    
-    // // -- Now refine the tolerance in Y
-    // if ( ut_hits.isNotYCompatible( layer_offset + i, yApprox, PrVeloUTConst::yTol + PrVeloUTConst::yTolSlope * std::abs(dx*invNormFact)) ) continue;
+  // const float normFactNum = normFact[layer];
+  // const float invNormFact = 1.0/normFactNum;
+  // xTol*invNormFact
+  // const auto zInit = ut_hits.zAtYEq0[layer_offset + posBeg];
+  // const auto xOnTrackProto = myState.x + myState.tx*(zInit - myState.z);
+  // const auto yApprox = myState.y + myState.ty * (zInit - myState.z);
+  // const auto xx = ut_hits.xAt(layer_offset + i, yApprox, dxDy); 
+  // const auto dx = xx - xOnTrackProto;
+  
+  // if( dx < -xTolNormFact ) continue;
+  // if( dx >  xTolNormFact ) continue; 
+  
+  // // -- Now refine the tolerance in Y
+  // if ( ut_hits.isNotYCompatible( layer_offset + i, yApprox, PrVeloUTConst::yTol + PrVeloUTConst::yTolSlope * std::abs(dx*invNormFact)) ) continue;
 
 
   for (int i_hit0 = from0; i_hit0 < to0; ++i_hit0) {
@@ -255,22 +253,19 @@ __host__ __device__ std::tuple<int,int,int,int> find_best_hits(
     const float yy0 = yyProto + (velo_state.ty * ut_hits.zAtYEq0[i_hit0]);
     x_hit_layer[0] = ut_hits.xAt(i_hit0, yy0, ut_dxDy[layers[0]]);
     const float zhitLayer0 = ut_hits.zAtYEq0[i_hit0];
-    hit0 = i_hit0;
+    best_hits[0] = i_hit0;
 
     for (int i_hit2 = from2; i_hit2 < to2; ++i_hit2) {
 
       const float yy2 = yyProto + (velo_state.ty * ut_hits.zAtYEq0[i_hit2]);
       x_hit_layer[2] = ut_hits.xAt(i_hit2, yy2, ut_dxDy[layers[2]]);
       const float zhitLayer2 = ut_hits.zAtYEq0[i_hit2];
-      hit2 = i_hit2;
+      best_hits[2] = i_hit2;
 
       // same bool check for the hit
       const float tx = (x_hit_layer[2] - x_hit_layer[0]) / (zhitLayer2 - zhitLayer0);
       if (std::abs(tx - velo_state.tx) <= PrVeloUTConst::deltaTx2) {
         float hitTol = PrVeloUTConst::hitTol2;
-
-        // int index_best_hit_1 = -1;
-        // int index_best_hit_3 = -1;
 
         // Search for triplet
         for (int i_hit1 = from1; i_hit1 < to1; ++i_hit1) {
@@ -281,7 +276,7 @@ __host__ __device__ std::tuple<int,int,int,int> find_best_hits(
           if (std::abs(x_hit_layer[1] - xextrapLayer1) < hitTol) {
             hitTol = std::abs(x_hit_layer[1] - xextrapLayer1);
             // index_best_hit_1 = i_hit1;
-            hit1 = i_hit1;
+            best_hits[1] = i_hit1;
           }
         }
 
@@ -295,11 +290,12 @@ __host__ __device__ std::tuple<int,int,int,int> find_best_hits(
           if (std::abs(x_hit_layer[3] - xextrapLayer3) < hitTol) {
             hitTol = std::abs(x_hit_layer[3] - xextrapLayer3);
             // index_best_hit_3 = i_hit3;
-            hit3 = i_hit3;
+            best_hits[3] = i_hit3;
           }
         }
-
+        
         // // Simple fit just one call with the 4 ints
+        BestParams best_params = pkick_fit(best_hits, ut_hits, velo_state, ut_dxDy, yyProto);
         // const float fit_result = fit(i_hit0, i_hit2, index_best_hit_1, index_best_hit_3);
 
         // if (fit_result.chi2 < fit.chi2) { // just check chi2
@@ -314,12 +310,122 @@ __host__ __device__ std::tuple<int,int,int,int> find_best_hits(
     }
   }
 
+  if (best_hits[0] != -1 && best_hits[1] != -1 && best_hits[2] != -1 && best_hits[3] != -1) {
+    printf("hit0: %i, hit1: %i, hit2: %i, hit3: %i\n", best_hits[0], best_hits[1], best_hits[2], best_hits[3]);  
+  }
+
   // return the found hits
-  return {hit0, hit1, hit2, hit3};
+  return {best_hits[0], best_hits[1], best_hits[2], best_hits[3]};
   // return {hit0, hit1, hit2, hit3, fit}; // also return the fit or the chi2
 
   // how to return
   // https://stackoverflow.com/questions/16713245/whats-the-best-way-to-return-a-tuple-from-function-in-c11
   // return std::make_tuple(i_hit0, index_best_hit_1, i_hit2, index_best_hit_3);
   // return std::forward_as_tuple(i_hit0, index_best_hit_1, i_hit2, index_best_hit_3);
+}
+
+//=========================================================================
+// apply the p-kick method to the triplet/quadruplet
+// TODO return the chi2?
+// TODO precalculate zDiff (its always the same)
+//=========================================================================
+__host__ __device__ BestParams pkick_fit(
+  const int best_hits[N_LAYERS],
+  const UTHits& ut_hits,
+  const MiniState& velo_state,
+  const float* ut_dxDy,
+  const float yyProto)
+{
+  BestParams best_params;
+
+  // accumulate the high threshold
+  int n_high_thres = 0;
+  for (int i = 0; i < N_LAYERS; ++i) {
+    if (best_hits[i] >= 0) { n_high_thres += ut_hits.highThreshold[best_hits[i]]; }
+  }
+
+  // Veto hit combinations with no high threshold hit
+  // = likely spillover
+  if (n_high_thres < PrVeloUTConst::minHighThres) return best_params;
+
+  // Scale the z-component, to not run into numerical problems with floats
+  // first add to sum values from hit at xMidField, zMidField hit
+  const float zDiff = 0.001 * (PrVeloUTConst::zKink - PrVeloUTConst::zMidUT);
+
+  // Helper stuff from velo state
+  const float xMidField = velo_state.x + velo_state.tx * (PrVeloUTConst::zKink - velo_state.z);
+  const float a = PrVeloUTConst::sigmaVeloSlope * (PrVeloUTConst::zKink - velo_state.z);
+  const float wb = 1. / (a * a);
+
+  float mat[3] = {wb, wb * zDiff, wb * zDiff * zDiff};
+  float rhs[2] = {wb * xMidField, wb * xMidField * zDiff};
+
+  // add hits
+  for (int i = 0; i < N_LAYERS; ++i) {
+    int hit_index = best_hits[i];
+    if (hit_index >= 0) {
+
+      const float wi = ut_hits.weight[hit_index];
+      const float dxDy = ut_dxDy[ut_hits.planeCode[hit_index]];
+      const float ci = ut_hits.cosT(hit_index, dxDy);
+      const float dz = 0.001 * (ut_hits.zAtYEq0[hit_index] - PrVeloUTConst::zMidUT);
+      // x_pos_layer
+      const float yy = yyProto + (velo_state.ty * ut_hits.zAtYEq0[hit_index]);
+      const float ui = ut_hits.xAt(hit_index, yy, dxDy);
+
+      mat[0] += wi * ci;
+      mat[1] += wi * ci * dz;
+      mat[2] += wi * ci * dz * dz;
+      rhs[0] += wi * ui;
+      rhs[1] += wi * ui * dz;
+    }
+  }
+
+  const float denom = 1. / (mat[0] * mat[2] - mat[1] * mat[1]);
+  const float xSlopeUTFit = 0.001 * (mat[0] * rhs[1] - mat[1] * rhs[0]) * denom;
+  const float xUTFit = (mat[2] * rhs[0] - mat[1] * rhs[1]) * denom;
+
+  // new VELO slope x
+  const float xb = xUTFit + xSlopeUTFit * (PrVeloUTConst::zKink - PrVeloUTConst::zMidUT);
+  const float invKinkVeloDist = 1 / (PrVeloUTConst::zKink - velo_state.z);
+  const float xSlopeVeloFit = (xb - velo_state.x) * invKinkVeloDist;
+  const float chi2VeloSlope = (velo_state.tx - xSlopeVeloFit) * PrVeloUTConst::invSigmaVeloSlope;
+
+  // chi2 takes chi2 from velo fit + chi2 from UT fit
+  float chi2UT = chi2VeloSlope * chi2VeloSlope;
+  // add chi2
+  int total_num_hits = 0;
+  for (int i = 0; i < N_LAYERS; ++i) {
+    int hit_index = best_hits[i];
+    if (hit_index >= 0) {
+      const float zd = ut_hits.zAtYEq0[hit_index];
+      const float xd = xUTFit + xSlopeUTFit * (zd - PrVeloUTConst::zMidUT);
+      // x_pos_layer
+      const float dxDy = ut_dxDy[ut_hits.planeCode[hit_index]];
+      const float yy = yyProto + (velo_state.ty * ut_hits.zAtYEq0[hit_index]);
+      const float x = ut_hits.xAt(hit_index, yy, dxDy);
+
+      const float du = xd - x;
+      chi2UT += (du * du) * ut_hits.weight[hit_index];
+
+      // count the number of processed htis
+      total_num_hits++;
+    }
+  }
+
+  chi2UT /= (total_num_hits - 1);
+
+  // Save the best parameters if chi2 is good
+  if (chi2UT < PrVeloUTConst::maxPseudoChi2) {
+    // calculate q/p
+    const float sinInX = xSlopeVeloFit * std::sqrt(1. + xSlopeVeloFit * xSlopeVeloFit);
+    const float sinOutX = xSlopeUTFit * std::sqrt(1. + xSlopeUTFit * xSlopeUTFit);
+
+    best_params.qp = sinInX - sinOutX;
+    best_params.chi2UT = chi2UT;
+    best_params.xUTFit = xUTFit;
+    best_params.xSlopeUTFit = xSlopeUTFit;
+  }
+
+  return best_params;
 }
