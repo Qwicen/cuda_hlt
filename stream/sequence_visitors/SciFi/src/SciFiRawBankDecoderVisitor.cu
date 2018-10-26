64efff00fcf7753bdc92e2a138eb4918d27e2139
Lars Funke <lars.funke@cern.ch>
2018-10-26 14:32:55 +0200

    Fix debugging

--- stream/sequence_visitors/SciFi/src/SciFiRawBankDecoderVisitor.cu
+++ stream/sequence_visitors/SciFi/src/SciFiRawBankDecoderVisitor.cu
@@ -26,32 +26,29 @@ void SequenceVisitor::visit<scifi_raw_bank_decoder_t>(
   );
 
   state.invoke();
-  
-  // // SciFi Decoder Debugging
-  // const uint hit_count_uints = 2 * number_of_events * SciFi::number_of_zones + 1;
+
+  // SciFi Decoder Debugging
+  // const uint hit_count_uints = 2 * runtime_options.number_of_events * SciFi::number_of_mats + 1;
   // uint host_scifi_hit_count[hit_count_uints];
-  // char* host_scifi_hits = new char[hits_bytes];
-  // uint* host_scifi_hit_permutation = new uint[*host_accumulated_number_of_scifi_hits];
-  // cudaCheck(cudaMemcpyAsync(&host_scifi_hit_count, arguments.offset<arg::dev_scifi_hit_count>(), hit_count_uints*sizeof(uint), cudaMemcpyDeviceToHost, stream));
-  // cudaCheck(cudaMemcpyAsync(host_scifi_hits, arguments.offset<arg::dev_scifi_hits>(), arguments.size<hits_bytes>(), cudaMemcpyDeviceToHost, stream));
-  // cudaCheck(cudaMemcpyAsync(host_scifi_hit_permutation, arguments.offset<arg::dev_scifi_hit_permutations>(), arguments.offset<arg::dev_scifi_hit_permutations>(), cudaMemcpyDeviceToHost, stream));
-  // cudaEventRecord(cuda_generic_event, stream);
+  // char* host_scifi_hits = new char[host_buffers.scifi_hits_bytes()];
+  // uint* host_scifi_hit_permutation = new uint[*host_buffers.host_accumulated_number_of_scifi_hits];
+  // cudaCheck(cudaMemcpyAsync(&host_scifi_hit_count, arguments.offset<arg::dev_scifi_hit_count>(), hit_count_uints*sizeof(uint), cudaMemcpyDeviceToHost, cuda_stream));
+  // cudaCheck(cudaMemcpyAsync(host_scifi_hits, arguments.offset<arg::dev_scifi_hits>(), arguments.size<arg::dev_scifi_hits>(), cudaMemcpyDeviceToHost, cuda_stream));
+  // cudaCheck(cudaMemcpyAsync(host_scifi_hit_permutation, arguments.offset<arg::dev_scifi_hit_permutations>(), arguments.size<arg::dev_scifi_hit_permutations>(), cudaMemcpyDeviceToHost, cuda_stream));
+  // cudaEventRecord(cuda_generic_event, cuda_stream);
   // cudaEventSynchronize(cuda_generic_event);
-
-  // SciFi::SciFiHits host_scifi_hits_struct;
-  // host_scifi_hits_struct.typecast_sorted(host_scifi_hits, host_scifi_hit_count[number_of_events * SciFi::number_of_zones]);
-
+  //
+  // SciFi::SciFiHits host_scifi_hits_struct(host_scifi_hits, host_scifi_hit_count[runtime_options.number_of_events * SciFi::number_of_mats]);
+  //
   // //Print only non-empty hits
   // std::ofstream outfile("dump.txt");
   // SciFi::SciFiHitCount host_scifi_hit_count_struct;
-  // for(size_t event = 0; event < number_of_events; event++) {
-  //   host_scifi_hit_count_struct.typecast_ascifier_prefix_sum(host_scifi_hit_count, event, number_of_events);
+  // for(size_t event = 0; event < runtime_options.number_of_events; event++) {
+  //   host_scifi_hit_count_struct.typecast_after_prefix_sum(host_scifi_hit_count, event, runtime_options.number_of_events);
   //   for(size_t zone = 0; zone < SciFi::number_of_zones; zone++) {
-  //     for(size_t hit = 0; hit < host_scifi_hit_count_struct.n_hits_layers[zone]; hit++) {
-  //       auto h = host_scifi_hits_struct.getHit(host_scifi_hit_count_struct.layer_offsets[zone] + hit);
-  //       outfile << std::setprecision(8) << std::fixed << h.planeCode << " " << h.hitZone << " " << h.LHCbID << " "
-  //         << h.x0 << " " << h.z0 << " " << h.w<< " " << h.dxdy << " "
-  //         << h.dzdy << " " << h.yMin << " " << h.yMax  <<  std::endl;
+  //     for(size_t hit = 0; hit < host_scifi_hit_count_struct.zone_number_of_hits(zone); hit++) {
+  //       auto h = host_scifi_hits_struct.getHit(host_scifi_hit_count_struct.zone_offset(zone) + hit);
+  //       outfile << std::setprecision(8) << std::fixed << h.x0 << std::endl;
   //     }
   //   }
   // }
