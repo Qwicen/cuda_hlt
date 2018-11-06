ab11eef949f36e5d338b265b6cf12f70c34c5221
Daniel Campora <dcampora@cern.ch>
2018-11-06 12:03:56 +0100

    Preparing Dorothea's branch for MR.

--- checker/tracking/src/TrackChecker.cpp
+++ checker/tracking/src/TrackChecker.cpp
@@ -7,8 +7,8 @@
  * @author Manuel Schiller
  * @date 2018-02-19
  *
- * 2018-07 Dorothea vom Bruch: updated to run over different track types, 
- * use exact same categories as PrChecker2, 
+ * 2018-07 Dorothea vom Bruch: updated to run over different track types,
+ * use exact same categories as PrChecker2,
  * take input from Renato Quagliani's TrackerDumper
  */
 
@@ -16,13 +16,11 @@
 
 #include "TrackChecker.h"
 
-TrackChecker::~TrackChecker()
-{
+TrackChecker::~TrackChecker() {
   std::printf("%-50s: %9lu/%9lu %6.2f%% (%6.2f%%) ghosts\n",
-      "TrackChecker output",
-      m_nghosts, m_ntracks,
-      100.f * float(m_nghosts) / float(m_ntracks),
-      100.f * m_ghostperevent);
+              "TrackChecker output", m_nghosts, m_ntracks,
+              100.f * float(m_nghosts) / float(m_ntracks),
+              100.f * m_ghostperevent);
   m_categories.clear();
   std::printf("\n");
 
@@ -31,62 +29,60 @@ TrackChecker::~TrackChecker()
   const std::string name = "../output/PrCheckerPlots.root";
   TFile *f = new TFile(name.c_str(), "UPDATE");
   std::string dirName = m_trackerName;
-  if ( m_trackerName == "VeloUT" )
+  if (m_trackerName == "VeloUT")
     dirName = "Upstream";
   TDirectory *trackerDir = f->mkdir(dirName.c_str());
   trackerDir->cd();
-  for ( auto histo : histos.h_reconstructible_eta )
+  for (auto histo : histos.h_reconstructible_eta)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructible_p )
+  for (auto histo : histos.h_reconstructible_p)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructible_pt )
+  for (auto histo : histos.h_reconstructible_pt)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructible_phi )
+  for (auto histo : histos.h_reconstructible_phi)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructible_nPV )
+  for (auto histo : histos.h_reconstructible_nPV)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructed_eta )
+  for (auto histo : histos.h_reconstructed_eta)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructed_p )
+  for (auto histo : histos.h_reconstructed_p)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructed_pt )
+  for (auto histo : histos.h_reconstructed_pt)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructed_phi )
+  for (auto histo : histos.h_reconstructed_phi)
     histo.second.Write();
-  for ( auto histo : histos.h_reconstructed_nPV )
+  for (auto histo : histos.h_reconstructed_nPV)
     histo.second.Write();
   histos.h_ghost_nPV.Write();
   histos.h_total_nPV.Write();
-  
+
   f->Write();
   f->Close();
 #endif
 }
 
-void TrackChecker::TrackEffReport::operator()(const MCParticles& mcps)
-{
+void TrackChecker::TrackEffReport::operator()(const MCParticles &mcps) {
   // find number of MCPs within category
-  for (auto mcp: mcps) {
+  for (auto mcp : mcps) {
     if (m_accept(mcp)) {
       ++m_naccept, ++m_nacceptperevt;
     }
   }
 }
 
-void TrackChecker::TrackEffReport::operator()(
-					    const trackChecker::Tracks::const_reference& track,
-					    const MCParticles::const_reference& mcp,
-					    const float weight)
-{
+void TrackChecker::TrackEffReport::
+operator()(const trackChecker::Tracks::const_reference &track,
+           const MCParticles::const_reference &mcp, const float weight) {
 
-  if (!m_accept(mcp)) return;
+  if (!m_accept(mcp))
+    return;
   if (!m_keysseen.count(mcp.key)) {
     ++m_nfound, ++m_nfoundperevt;
     m_keysseen.insert(mcp.key);
   } else {
     ++m_nclones;
   }
-  
+
   // update purity
   m_hitpur *= float(m_nfound + m_nclones - 1) / float(m_nfound + m_nclones);
   m_hitpur += weight / float(m_nfound + m_nclones);
@@ -94,92 +90,98 @@ void TrackChecker::TrackEffReport::operator()(
   auto hiteff = track.n_matched_total * weight / float(mcp.numHits);
   m_hiteff *= float(m_nfound + m_nclones - 1) / float(m_nfound + m_nclones);
   m_hiteff += hiteff / float(m_nfound + m_nclones);
-
 }
 
-void TrackChecker::TrackEffReport::evtEnds()
-{
+void TrackChecker::TrackEffReport::evtEnds() {
   m_keysseen.clear();
   if (m_nacceptperevt) {
     m_effperevt *= float(m_nevents) / float(m_nevents + 1);
     ++m_nevents;
-    m_effperevt += (float(m_nfoundperevt) / float(m_nacceptperevt)) / float(m_nevents);
+    m_effperevt +=
+        (float(m_nfoundperevt) / float(m_nacceptperevt)) / float(m_nevents);
   }
   m_nfoundperevt = m_nacceptperevt = 0;
 }
 
-TrackChecker::TrackEffReport::~TrackEffReport()
-{
+TrackChecker::TrackEffReport::~TrackEffReport() {
   auto clonerate = 0.f, eff = 0.f;
-  if (m_nfound) clonerate = float(m_nclones) / float(m_nfound + m_nfound);
-  if (m_naccept) eff = float(m_nfound) / float(m_naccept);
+  if (m_nfound)
+    clonerate = float(m_nclones) / float(m_nfound + m_nfound);
+  if (m_naccept)
+    eff = float(m_nfound) / float(m_naccept);
 
   if (m_naccept > 0) {
     std::printf("%-50s: %9lu/%9lu %6.2f%% (%6.2f%%), "
-        "%9lu (%6.2f%%) clones, hit eff %6.2f%% pur %6.2f%%\n",
-        m_name.c_str(), m_nfound, m_naccept,
-        100.f * eff,
-        100.f * m_effperevt, m_nclones,
-        100.f * clonerate,
-        100.f * m_hiteff, 100.f * m_hitpur);
+                "%9lu (%6.2f%%) clones, hit eff %6.2f%% pur %6.2f%%\n",
+                m_name.c_str(), m_nfound, m_naccept, 100.f * eff,
+                100.f * m_effperevt, m_nclones, 100.f * clonerate,
+                100.f * m_hiteff, 100.f * m_hitpur);
   }
 }
 
-void TrackChecker::HistoCategory::evtEnds()
-{
-  m_keysseen.clear();
-}
+void TrackChecker::HistoCategory::evtEnds() { m_keysseen.clear(); }
 
+void TrackChecker::Histos::initHistos() {
 #ifdef WITH_ROOT
-void TrackChecker::initHistos() {
   // histos for efficiency
-  for ( auto histoCat : m_histo_categories ) {
-    const std::string category = histoCat.m_name;
+  for (auto histoCat : m_histo_categories) {
+    const std::string &category = histoCat.m_name;
     std::string name = category + "_Eta_reconstructible";
-    if ( category.find("eta25") != std::string::npos) {
-      histos.h_reconstructible_eta[name] = TH1D(name.c_str(), name.c_str(), 50, 0., 7.);
+    if (category.find("eta25") != std::string::npos) {
+      histos.h_reconstructible_eta[name] =
+          TH1D(name.c_str(), name.c_str(), 50, 0., 7.);
       name = category + "_Eta_reconstructed";
-      histos.h_reconstructed_eta[name] = TH1D(name.c_str(), name.c_str(), 50, 0., 7.);
-    }
-    else {
-      histos.h_reconstructible_eta[name] = TH1D(name.c_str(), name.c_str(), 100, -7., 7.);
+      histos.h_reconstructed_eta[name] =
+          TH1D(name.c_str(), name.c_str(), 50, 0., 7.);
+    } else {
+      histos.h_reconstructible_eta[name] =
+          TH1D(name.c_str(), name.c_str(), 100, -7., 7.);
       name = category + "_Eta_reconstructed";
-      histos.h_reconstructed_eta[name] = TH1D(name.c_str(), name.c_str(), 100, -7., 7.);
+      histos.h_reconstructed_eta[name] =
+          TH1D(name.c_str(), name.c_str(), 100, -7., 7.);
     }
     name = category + "_P_reconstructible";
-    histos.h_reconstructible_p[name] = TH1D(name.c_str(), name.c_str(), 50, 0.,100000.);
+    histos.h_reconstructible_p[name] =
+        TH1D(name.c_str(), name.c_str(), 50, 0., 100000.);
     name = category + "_Pt_reconstructible";
-    histos.h_reconstructible_pt[name] = TH1D(name.c_str(), name.c_str(), 50, 0.,100000.);
+    histos.h_reconstructible_pt[name] =
+        TH1D(name.c_str(), name.c_str(), 50, 0., 100000.);
     name = category + "_Phi_reconstructible";
-    histos.h_reconstructible_phi[name] = TH1D(name.c_str(), name.c_str(), 25, -3.142,3.142);
+    histos.h_reconstructible_phi[name] =
+        TH1D(name.c_str(), name.c_str(), 25, -3.142, 3.142);
     name = category + "_nPV_reconstructible";
-    histos.h_reconstructible_nPV[name] = TH1D(name.c_str(), name.c_str(), 21, -0.5,20.5);
+    histos.h_reconstructible_nPV[name] =
+        TH1D(name.c_str(), name.c_str(), 21, -0.5, 20.5);
     name = category + "_P_reconstructed";
-    histos.h_reconstructed_p[name] = TH1D(name.c_str(), name.c_str(), 50, 0., 100000.);
+    histos.h_reconstructed_p[name] =
+        TH1D(name.c_str(), name.c_str(), 50, 0., 100000.);
     name = category + "_Pt_reconstructed";
-    histos.h_reconstructed_pt[name] = TH1D(name.c_str(), name.c_str(), 50, 0., 100000.);
+    histos.h_reconstructed_pt[name] =
+        TH1D(name.c_str(), name.c_str(), 50, 0., 100000.);
     name = category + "_Phi_reconstructed";
-    histos.h_reconstructed_phi[name] = TH1D(name.c_str(), name.c_str(), 25, -3.142,3.142);
+    histos.h_reconstructed_phi[name] =
+        TH1D(name.c_str(), name.c_str(), 25, -3.142, 3.142);
     name = category + "_nPV_reconstructed";
-    histos.h_reconstructed_nPV[name] = TH1D(name.c_str(), name.c_str(), 21, -0.5,20.5);
+    histos.h_reconstructed_nPV[name] =
+        TH1D(name.c_str(), name.c_str(), 21, -0.5, 20.5);
   }
 
   // histos for ghost rate
-  histos.h_ghost_nPV = TH1D("nPV_Ghosts", "nPV_Ghosts", 21, -0.5,20.5);
-  histos.h_total_nPV = TH1D("nPV_Total", "nPV_Total", 21, -0.5,20.5);
+  histos.h_ghost_nPV = TH1D("nPV_Ghosts", "nPV_Ghosts", 21, -0.5, 20.5);
+  histos.h_total_nPV = TH1D("nPV_Total", "nPV_Total", 21, -0.5, 20.5);
+#endif
 }
 
-void TrackChecker::fillReconstructibleHistos(
-  const MCParticles& mcps,
-  const HistoCategory& category)
-{
+void TrackChecker::Histos::fillReconstructibleHistos(
+    const MCParticles &mcps, const HistoCategory &category) {
+#ifdef WITH_ROOT
   const std::string eta_name = category.m_name + "_Eta_reconstructible";
-  const std::string p_name   = category.m_name + "_P_reconstructible";
-  const std::string pt_name  = category.m_name + "_Pt_reconstructible";
+  const std::string p_name = category.m_name + "_P_reconstructible";
+  const std::string pt_name = category.m_name + "_Pt_reconstructible";
   const std::string phi_name = category.m_name + "_Phi_reconstructible";
   const std::string nPV_name = category.m_name + "_nPV_reconstructible";
-  for ( auto mcp : mcps ) {
-    if ( category.m_accept(mcp) ) {
+  for (auto mcp : mcps) {
+    if (category.m_accept(mcp)) {
       histos.h_reconstructible_eta[eta_name].Fill(mcp.eta);
       histos.h_reconstructible_p[p_name].Fill(mcp.p);
       histos.h_reconstructible_pt[pt_name].Fill(mcp.pt);
@@ -187,19 +189,21 @@ void TrackChecker::fillReconstructibleHistos(
       histos.h_reconstructible_nPV[nPV_name].Fill(mcp.nPV);
     }
   }
+#endif
 }
 
-void TrackChecker::fillReconstructedHistos(
-  const MCParticle& mcp,
-  HistoCategory& category)           
-{
-  if ( !( category.m_accept(mcp) ) ) return;
-  if ( (category.m_keysseen).count(mcp.key) ) return; // clone track
+void TrackChecker::Histos::fillReconstructedHistos(const MCParticle &mcp,
+                                                   HistoCategory &category) {
+#ifdef WITH_ROOT
+  if (!(category.m_accept(mcp)))
+    return;
+  if ((category.m_keysseen).count(mcp.key))
+    return;                              // clone track
   (category.m_keysseen).insert(mcp.key); // not clone track, mark as matched
-  
+
   const std::string eta_name = category.m_name + "_Eta_reconstructed";
-  const std::string p_name   = category.m_name + "_P_reconstructed";
-  const std::string pt_name  = category.m_name + "_Pt_reconstructed";
+  const std::string p_name = category.m_name + "_P_reconstructed";
+  const std::string pt_name = category.m_name + "_Pt_reconstructed";
   const std::string phi_name = category.m_name + "_Phi_reconstructed";
   const std::string nPV_name = category.m_name + "_nPV_reconstructed";
   histos.h_reconstructed_eta[eta_name].Fill(mcp.eta);
@@ -207,74 +211,73 @@ void TrackChecker::fillReconstructedHistos(
   histos.h_reconstructed_pt[pt_name].Fill(mcp.pt);
   histos.h_reconstructed_phi[phi_name].Fill(mcp.phi);
   histos.h_reconstructed_nPV[nPV_name].Fill(mcp.nPV);
-}
-
-void TrackChecker::fillTotalHistos( const MCParticle& mcp ) {
-  histos.h_total_nPV.Fill(mcp.nPV);
-}
-
-void TrackChecker::fillGhostHistos( const MCParticle& mcp ) {
-  histos.h_ghost_nPV.Fill(mcp.nPV);
-}
 #endif
+}
 
-void TrackChecker::operator()(const trackChecker::Tracks& tracks,
-    const MCAssociator& mcassoc, const MCParticles& mcps)
-{
-  // register MC particles
-  for (auto& report: m_categories) report(mcps);
-  // fill histograms of reconstructible MC particles in various categories
+void TrackChecker::Histos::fillTotalHistos(const MCParticle &mcp) {
 #ifdef WITH_ROOT
-  for ( auto& histo_cat : m_histo_categories )
-    fillReconstructibleHistos(mcps, histo_cat);
+  histos.h_total_nPV.Fill(mcp.nPV);
 #endif
-            
+}
+
+void TrackChecker::Histos::fillGhostHistos(const MCParticle &mcp) {
+#ifdef WITH_ROOT
+  histos.h_ghost_nPV.Fill(mcp.nPV);
+#endif
+}
+
+void TrackChecker::operator()(const trackChecker::Tracks &tracks,
+                              const MCAssociator &mcassoc,
+                              const MCParticles &mcps) {
+  // register MC particles
+  for (auto &report : m_categories)
+    report(mcps);
+  // fill histograms of reconstructible MC particles in various categories
+  for (auto &histo_cat : m_histo_categories) {
+    histos.fillReconstructibleHistos(mcps, histo_cat);
+  }
+
   // go through tracks
   const std::size_t ntracksperevt = tracks.size();
   std::size_t nghostsperevt = 0;
-  for (auto track: tracks) {
-#ifdef WITH_ROOT
-    fillTotalHistos(mcps[0]);
-#endif
+  for (auto track : tracks) {
+    histos.fillTotalHistos(mcps[0]);
     // check LHCbIDs for MC association
-    const auto& ids = track.ids();
+    const auto &ids = track.ids();
     const auto assoc = mcassoc(ids.begin(), ids.end(), track.n_matched_total);
     if (!assoc) {
       ++nghostsperevt;
-#ifdef WITH_ROOT
-      fillGhostHistos(mcps[0]);
-#endif
-	  continue;
+      histos.fillGhostHistos(mcps[0]);
+      continue;
     }
     // have MC association, check weight
     const auto weight = assoc.front().second;
     if (weight < m_minweight) {
       ++nghostsperevt;
-#ifdef WITH_ROOT
-      fillGhostHistos(mcps[0]);
-#endif
+      histos.fillGhostHistos(mcps[0]);
       continue;
     }
     // okay, sufficient to proceed...
     const auto mcp = assoc.front().first;
     // add to various categories
-    for (auto& report: m_categories) {
+    for (auto &report : m_categories) {
       report(track, mcp, weight);
     }
-      // fill histograms of reconstructible MC particles in various categories
-#ifdef WITH_ROOT
-    for ( auto& histo_cat : m_histo_categories ) {
-      fillReconstructedHistos(mcp, histo_cat);
+    // fill histograms of reconstructible MC particles in various categories
+    for (auto &histo_cat : m_histo_categories) {
+      histos.fillReconstructedHistos(mcp, histo_cat);
     }
-#endif
   }
   // almost done, notify of end of event...
   ++m_nevents;
-  for (auto& report: m_categories) report.evtEnds();
-  for (auto& histo_cat: m_histo_categories) histo_cat.evtEnds();
+  for (auto &report : m_categories)
+    report.evtEnds();
+  for (auto &histo_cat : m_histo_categories)
+    histo_cat.evtEnds();
   m_ghostperevent *= float(m_nevents - 1) / float(m_nevents);
   if (ntracksperevt) {
-    m_ghostperevent += (float(nghostsperevt) / float(ntracksperevt)) / float(m_nevents);
+    m_ghostperevent +=
+        (float(nghostsperevt) / float(ntracksperevt)) / float(m_nevents);
   }
   m_nghosts += nghostsperevt, m_ntracks += ntracksperevt;
 }
