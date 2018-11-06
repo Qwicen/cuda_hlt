79834e5d35597ad0465481a3572755a997688833
Daniel Campora <dcampora@cern.ch>
2018-11-06 15:32:03 +0100

    Revert "Merge branch 'revert-1cd813c6' into 'master'"
    This reverts commit fa3e1b6b4f31731de31f818e890b3fd2df4d313b, reversing
    changes made to 1cd813c6fcf77522a9143dffc6ee79f0f4e7a5d4.

--- checker/tracking/include/TrackChecker.h
+++ checker/tracking/include/TrackChecker.h
@@ -8,7 +8,7 @@
  * @date 2018-02-19
  *
  * 2018-07 Dorothea vom Bruch: updated to run over different track types, 
- * euse exact same categories as PrChecker2, 
+ * use exact same categories as PrChecker2, 
  * take input from Renato Quagliani's TrackerDumper
  */
 
@@ -22,6 +22,12 @@
 #include "Tracks.h"
 #include "MCAssociator.h"
 #include "Logger.h"
+
+#ifdef WITH_ROOT
+#include "TH1D.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#endif
 
 class TrackChecker
 {
@@ -64,7 +70,7 @@ class TrackChecker
             /// register MC particles
             void operator()(const MCParticles& mcps);
             /// register track and its MC association
-	  void operator()(trackChecker::Tracks::const_reference& track,
+	    void operator()(trackChecker::Tracks::const_reference& track,
                     MCParticles::const_reference& mcp,
                     const float weight);
             /// notify of end of event
@@ -73,38 +79,100 @@ class TrackChecker
             ~TrackEffReport();
         };
 
-        const float m_minweight = 0.7f;
+        struct HistoCategory {
+          std::string m_name;
+          AcceptFn m_accept;
+          std::set<uint32_t> m_keysseen;
+          
+          /// construction from name and accept criterion for eff. denom.
+          template <typename F>
+          HistoCategory(const std::string& name, const F& accept) :
+            m_name(name), m_accept(accept)
+          {}
+          /// construction from name and accept criterion for eff. denom.
+          template <typename F>
+          HistoCategory(std::string&& name, F&& accept) :
+            m_name(std::move(name)), m_accept(std::move(accept))
+          {}
+          /// notify of end of event
+          void evtEnds();
+        };
+                
         std::vector<TrackEffReport> m_categories;
+        std::vector<HistoCategory> m_histo_categories;
+        std::string m_trackerName = "";
 
+        struct Histos {
+#ifdef WITH_ROOT
+          std::map< std::string, TH1D > h_reconstructible_eta;
+          std::map< std::string, TH1D > h_reconstructible_p;
+          std::map< std::string, TH1D > h_reconstructible_pt;
+          std::map< std::string, TH1D > h_reconstructible_phi;
+          std::map< std::string, TH1D > h_reconstructible_nPV;
+          std::map< std::string, TH1D > h_reconstructed_eta;
+          std::map< std::string, TH1D > h_reconstructed_p;
+          std::map< std::string, TH1D > h_reconstructed_pt;
+          std::map< std::string, TH1D > h_reconstructed_phi;
+          std::map< std::string, TH1D > h_reconstructed_nPV;
+
+          TH1D h_ghost_nPV;
+          TH1D h_total_nPV;
+#endif
+        };
+
+        Histos histos;
+        
+        const float m_minweight = 0.7f;
         std::size_t m_nevents = 0;
         std::size_t m_ntracks = 0;
         std::size_t m_nghosts = 0;
         float m_ghostperevent = 0.f;
-
+        
+        virtual void SetHistoCategories() = 0;
+        virtual void SetCategories() = 0;
+        
     public:
-        TrackChecker() {};
+        TrackChecker() {
+        };
         ~TrackChecker();
         void operator()(const trackChecker::Tracks& tracks,
-                const MCAssociator& mcassoc,
-                const MCParticles& mcps);
+          const MCAssociator& mcassoc,
+          const MCParticles& mcps);
+#ifdef WITH_ROOT
+        void initHistos();
+        void fillReconstructibleHistos(
+          const MCParticles& mcps,
+          const HistoCategory& category);
+        void fillReconstructedHistos(
+          const MCParticle& mcp,
+          HistoCategory& category);
+        void fillTotalHistos( const MCParticle& mcp );
+        void fillGhostHistos( const MCParticle& mcp );
+#endif                
 };
 
 class TrackCheckerVelo : public TrackChecker
 {
   public:
       void SetCategories();
+      void SetHistoCategories();
       TrackCheckerVelo() {
         SetCategories();
+        SetHistoCategories();
+        m_trackerName = "Velo";
       };
-  
+        
 };
 
 class TrackCheckerVeloUT : public TrackChecker
 {
   public:
       void SetCategories();
+      void SetHistoCategories();
       TrackCheckerVeloUT() {
         SetCategories();
+        SetHistoCategories();
+        m_trackerName = "VeloUT";
       };
   
 };
@@ -113,8 +181,13 @@ class TrackCheckerForward : public TrackChecker
 {
   public:
       void SetCategories();
+      void SetHistoCategories();
       TrackCheckerForward() {
         SetCategories();
+        SetHistoCategories();
+        m_trackerName = "Forward";
       };
   
-}; 
+};
+
+
