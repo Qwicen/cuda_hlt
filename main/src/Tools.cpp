3cffebaf02729fdba96759aa63a0df948f339fa2
Sergey Popov <popovsergey95@gmail.com>
2018-11-19 16:38:25 +0300

    fix muon common hits reading

--- main/src/Tools.cpp
+++ main/src/Tools.cpp
@@ -79,7 +79,7 @@ void read_muon_events_into_arrays(
     muon_station_hits[i_event].station_offsets[0] = 0;
     for(int i_station = 1; i_station < Muon::Constants::n_stations; ++i_station) {
       muon_station_hits[i_event].station_offsets[i_station] = muon_station_hits[i_event].station_offsets[i_station - 1]
-                                                            + muon_station_hits[i_event].number_of_hits_per_station[i_event - 1];
+                                                            + muon_station_hits[i_event].number_of_hits_per_station[i_station - 1];
     }
     for(int i_station = 0; i_station < Muon::Constants::n_stations; ++i_station) {
       const int station_offset = muon_station_hits[i_event].station_offsets[i_station];
@@ -87,17 +87,17 @@ void read_muon_events_into_arrays(
       std::copy_n((int*) raw_input, number_of_hits, &( muon_station_hits[i_event].tile[station_offset]) );
       raw_input += sizeof(int) * number_of_hits;
       std::copy_n((float*) raw_input, number_of_hits, &( muon_station_hits[i_event].x[station_offset]) );
-      raw_input += sizeof(int) * number_of_hits;
+      raw_input += sizeof(float) * number_of_hits;
       std::copy_n((float*) raw_input, number_of_hits, &( muon_station_hits[i_event].dx[station_offset]) );
-      raw_input += sizeof(int) * number_of_hits;
+      raw_input += sizeof(float) * number_of_hits;
       std::copy_n((float*) raw_input, number_of_hits, &( muon_station_hits[i_event].y[station_offset]) );
-      raw_input += sizeof(int) * number_of_hits;
+      raw_input += sizeof(float) * number_of_hits;
       std::copy_n((float*) raw_input, number_of_hits, &( muon_station_hits[i_event].dy[station_offset]) );
-      raw_input += sizeof(int) * number_of_hits;
+      raw_input += sizeof(float) * number_of_hits;
       std::copy_n((float*) raw_input, number_of_hits, &( muon_station_hits[i_event].z[station_offset]) );
-      raw_input += sizeof(int) * number_of_hits;
+      raw_input += sizeof(float) * number_of_hits;
       std::copy_n((float*) raw_input, number_of_hits, &( muon_station_hits[i_event].dz[station_offset]) );
-      raw_input += sizeof(int) * number_of_hits;
+      raw_input += sizeof(float) * number_of_hits;
       std::copy_n((int*) raw_input, number_of_hits, &( muon_station_hits[i_event].uncrossed[station_offset]) );
       raw_input += sizeof(int) * number_of_hits;
       std::copy_n((unsigned int*) raw_input, number_of_hits, &( muon_station_hits[i_event].time[station_offset]) );
