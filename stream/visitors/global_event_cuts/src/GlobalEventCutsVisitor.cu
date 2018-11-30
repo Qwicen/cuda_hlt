933a87384eb5c94efc45db1b7bdeb5bc855c78bc
Dorothea vom Bruch <dorothea.vom.bruch@cern.ch>
2018-11-30 09:36:33 +0100

    parallelize gec: one thread per raw bank for both the UT and SciFi banks

--- stream/visitors/global_event_cuts/src/GlobalEventCutsVisitor.cu
+++ stream/visitors/global_event_cuts/src/GlobalEventCutsVisitor.cu
@@ -33,7 +33,7 @@ void SequenceVisitor::visit<global_event_cuts_t>(
     cuda_stream));
   
   // Setup opts and arguments for kernel call
-  state.set_opts(dim3(runtime_options.number_of_events), dim3(1), cuda_stream);
+  state.set_opts(dim3(runtime_options.number_of_events), dim3(32), cuda_stream);
   state.set_arguments(
     arguments.offset<dev_raw_input>(),
     arguments.offset<dev_raw_input_offsets>(),
@@ -52,7 +52,20 @@ void SequenceVisitor::visit<global_event_cuts_t>(
     sizeof(uint),
     cudaMemcpyDeviceToHost, 
     cuda_stream));
+
+  // cudaCheck(cudaMemcpyAsync(
+  //   host_buffers.host_event_list,
+  //   arguments.offset<dev_event_list>(),
+  //   runtime_options.number_of_events*sizeof(uint),
+  //   cudaMemcpyDeviceToHost, 
+  //   cuda_stream));
     
   cudaEventRecord(cuda_generic_event, cuda_stream);
   cudaEventSynchronize(cuda_generic_event);
+  
+  info_cout << "Selected " << host_buffers.host_number_of_selected_events[0] << " / " << runtime_options.number_of_events << " events with global event cuts" << std::endl;
+  // for ( int i = 0; i < host_buffers.host_number_of_selected_events[0]; ++i ) {
+  //   debug_cout << host_buffers.host_event_list[i] << std::endl;
+  // }
+
 }
