37aa2f6a9c0ef068f654f50dff9d151a502dc212
Dorothea vom Bruch <dorothea.vom.bruch@cern.ch>
2018-12-01 21:28:56 +0100

    copy back event list also in init_event_list for non GEC case

--- stream/visitors/global_event_cuts/src/InitEventListVisitor.cu
+++ stream/visitors/global_event_cuts/src/InitEventListVisitor.cu
@@ -69,4 +69,12 @@ void SequenceVisitor::visit<init_event_list_t>(
     cuda_stream)); 
 
   state.invoke();
+
+  cudaCheck(cudaMemcpyAsync(
+    host_buffers.host_event_list,
+    arguments.offset<dev_event_list>(),
+    runtime_options.number_of_events*sizeof(uint),
+    cudaMemcpyDeviceToHost, 
+    cuda_stream));
+
 }
