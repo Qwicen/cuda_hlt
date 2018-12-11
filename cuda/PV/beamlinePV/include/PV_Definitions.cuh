345637808510e50ef10010014466bde7661f6d6a
Florian Reiss <freiss@cern.ch>
2018-12-11 16:08:11 +0100

    change constants to explicitly be floats

--- cuda/PV/beamlinePV/include/PV_Definitions.cuh
+++ cuda/PV/beamlinePV/include/PV_Definitions.cuh
@@ -24,8 +24,8 @@ static constexpr uint max_number_clusteredges = 200;
 
 // configuration for seeding
 // steering parameters for merging procedure
-static constexpr myfloat mcu_maxChi2Merge = 25.;
-static constexpr myfloat mcu_factorToIncreaseErrors = 15.;
+static constexpr myfloat mcu_maxChi2Merge = 25.f;
+static constexpr myfloat mcu_factorToIncreaseErrors = 15.f;
 
 // try parameters from RecoUpgradeTracking.py
 static constexpr int mcu_minClusterMult = 4;
@@ -33,42 +33,42 @@ static constexpr int mcu_minCloseTracksInCluster = 3;
 
 // steering parameters for final cluster selection
 // int    m_minClusterMult = 3;
-static constexpr myfloat mcu_dzCloseTracksInCluster = 5.; // unit: mm
+static constexpr myfloat mcu_dzCloseTracksInCluster = 5.f; // unit: mm
 // int    m_minCloseTracksInCluster = 3;
 static constexpr int mcu_highMult = 10;
-static constexpr myfloat mcu_ratioSig2HighMult = 1.0;
-static constexpr myfloat mcu_ratioSig2LowMult = 0.9;
+static constexpr myfloat mcu_ratioSig2HighMult = 1.0f;
+static constexpr myfloat mcu_ratioSig2LowMult = 0.9f;
 
 static constexpr int mcu_max_clusters = 1200; // maximmum number of clusters
 
-static constexpr myfloat mcu_x0MS = 0.01; // X0 (tunable) of MS to add for extrapolation of
+static constexpr myfloat mcu_x0MS = 0.01f; // X0 (tunable) of MS to add for extrapolation of
                                           // track parameters to PV
 
 // don't forget to actually calculate this!!
 // double  m_scatCons = 0;     // calculated from m_x0MS
-static constexpr myfloat X0cu = 0.01;
+static constexpr myfloat X0cu = 0.01f;
 // static constexpr  double m_scatCons = (13.6*sqrt(X0)*(1.+0.038*log(X0)));
-static constexpr myfloat mcu_scatCons = 0.01;
+static constexpr myfloat mcu_scatCons = 0.01f;
 
 // configuration for fitting seeds
 static constexpr size_t m_minTr = 4;
 static constexpr int m_Iterations = 20;
 static constexpr int m_minIter = 5;
-static constexpr myfloat m_maxDeltaZ = 0.0005; // unit:: mm
-static constexpr myfloat m_minTrackWeight = 0.00000001;
-static constexpr myfloat m_TrackErrorScaleFactor = 1.0;
-static constexpr myfloat m_maxChi2 = 400.0;
-static constexpr myfloat m_trackMaxChi2 = 12.;
+static constexpr myfloat m_maxDeltaZ = 0.0005f; // unit:: mm
+static constexpr myfloat m_minTrackWeight = 0.00000001f;
+static constexpr myfloat m_TrackErrorScaleFactor = 1.0f;
+static constexpr myfloat m_maxChi2 = 400.0f;
+static constexpr myfloat m_trackMaxChi2 = 12.f;
 // static constexpr  double m_trackChi = std::sqrt(m_trackMaxChi2);     // sqrt of trackMaxChi2
-static constexpr myfloat m_trackChi = 3.464; // sqrt of trackMaxChi2
-static constexpr myfloat m_trackMaxChi2Remove = 25.;
-static constexpr myfloat m_maxDeltaZCache = 1.; // unit: mm
+static constexpr myfloat m_trackChi = 3.464f; // sqrt of trackMaxChi2
+static constexpr myfloat m_trackMaxChi2Remove = 25.f;
+static constexpr myfloat m_maxDeltaZCache = 1.f; // unit: mm
 
 struct vtxCluster final {
 
-  myfloat z = 0;        // z of the cluster
-  myfloat sigsq = 0;    // sigma**2 of the cluster
-  myfloat sigsqmin = 0; // minimum sigma**2 of the tracks forming cluster
+  myfloat z = 0.f;        // z of the cluster
+  myfloat sigsq = 0.f;    // sigma**2 of the cluster
+  myfloat sigsqmin = 0.f; // minimum sigma**2 of the tracks forming cluster
   int ntracks = 1;      // number of tracks in the cluster
   bool merged = false;  // flag for iterative merging
 
@@ -81,14 +81,14 @@ public:
   float3 position;
   myfloat chi2;
   int ndof;
-  uint n_tracks = 0;
+  uint n_tracks = 0.f;
 
-  myfloat cov00 = 0.;
-  myfloat cov10 = 0.;
-  myfloat cov11 = 0.;
-  myfloat cov20 = 0.;
-  myfloat cov21 = 0.;
-  myfloat cov22 = 0.;
+  myfloat cov00 = 0.f;
+  myfloat cov10 = 0.f;
+  myfloat cov11 = 0.f;
+  myfloat cov20 = 0.f;
+  myfloat cov21 = 0.f;
+  myfloat cov22 = 0.f;
 
   __host__ __device__ void setChi2AndDoF(myfloat m_chi2, int m_ndof)
   {
@@ -105,7 +105,7 @@ public:
     cov21 = m_cov[4];
     cov22 = m_cov[5];
   }
-  int nTracks = 0.;
+  int nTracks = 0;
 };
 
 } // namespace PV
